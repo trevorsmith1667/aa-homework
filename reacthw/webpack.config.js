41ab7e1078e0609aef47494df45238bc8539ff3e
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-26 10:01:20 -0800

    m

---
+++ reacthw/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+},
+    resolve: {
+    extensions: ['.js', '.jsx', '*']
+},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+  
+};
+
+
